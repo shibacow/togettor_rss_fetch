87737acd296785384cf3552782ca5778e1d99ba2
shibacow <shibacow@gmail.com>
2022-08-12 19:59:07 +0900

    ch data

--- togetter_analisys.py
+++ togetter_analisys.py
@@ -6,11 +6,11 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 db=anydbm.open('togetter.dbm')
-s=db.keys()
-s=s[:100]
-for a in sorted(s):
-    c=db[a]
-    sz=len(c)
-    k=zlib.decompress(c)
-    logging.info(k[:1000])
+cnt=0
+for k in db.keys():
+    logging.info(k)
+    cnt+=1
+    if cnt>10:
+        break
+
     
